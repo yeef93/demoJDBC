1570be7dd2adc8a7d0427268bb2cdaeef940ac89
yeef93 <yunindafaranika@gmail.com>
2024-06-07 13:59:33 +0700

    initial commit

---
+++ src/test/java/com/yeef93/demoJDBC/DemoJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeef93.demoJDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
